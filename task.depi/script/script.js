b0498667a0dd470517017ad95ec81ac5590373ce
hams27 <152614131+hams27@users.noreply.github.com>
2024-09-07 14:42:18 +0200

    Update script.js

--- task.depi/script/script.js
+++ task.depi/script/script.js
@@ -1,7 +1,8 @@
 let username=document.getElementById('username');
 let password=document.getElementById('password');
 
-document.getElementById('submit').addEventListener("click",function(){
+document.getElementById('submit').addEventListener("click",function(e){
+    e.preventDefault()
    if(username.value !=='' && password.value !=='')
        {
            location.href="exam.html";
